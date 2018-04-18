37187ae56e2d9c5ba37644ddbdec20d6c6ad2cc0
Xiao-Yong Jin <xjin@anl.gov>
2018-04-18 14:51:19 -0500

    composition demo

--- mdevolve.nim
+++ mdevolve.nim
@@ -73,26 +73,61 @@ proc evolve*(S:FGYin11, t:float) =
   S.x.evolve h6
 
 when isMainModule:
-  import math
+  import math, os
 
+  let Verb = existsenv("VerbTest")
   # Simple 2D system
   type A = array[2,float]
-  proc V(x:A):float =
+  # Coulomb and Harmonic potential
+  proc Vc(x:A):float =
     let
       x0 = x[0]
       x1 = x[1]
       r2 = x0*x0+x1*x1
       r = sqrt r2
-    -1 / r
-  proc F(x:A):A =
+    -1 / r + r2 / 2.0
+  proc Fc(x:A):A =
     let
       x0 = x[0]
       x1 = x[1]
       r2 = x0*x0+x1*x1
       r = sqrt r2
       r3 = r*r2
-    result[0] = - x0 / r3
-    result[1] = - x1 / r3
+    result[0] = -(x0 + x0 / r3)
+    result[1] = -(x1 + x1 / r3)
+  # Separating the log potential for this demo
+  proc Vl0(x:A):float =
+    let
+      x0 = x[0]
+      x1 = x[1]
+      r2 = x0*x0+x1*x1
+      r = sqrt r2
+    -ln(r/(r+1))
+  proc Fl0(x:A):A =
+    let
+      x0 = x[0]
+      x1 = x[1]
+      r2 = x0*x0+x1*x1
+      r = sqrt r2
+      d = 1/(r2*(1+r))
+    result[0] = x0 * d
+    result[1] = x1 * d
+  proc Vl1(x:A):float =
+    let
+      x0 = x[0]
+      x1 = x[1]
+      r2 = x0*x0+x1*x1
+      r = sqrt r2
+    -ln(r+1)
+  proc Fl1(x:A):A =
+    let
+      x0 = x[0]
+      x1 = x[1]
+      r2 = x0*x0+x1*x1
+      r = sqrt r2
+      d = 1/(r2+r)
+    result[0] = x0 * d
+    result[1] = x1 * d
   proc K(p:A):float =
     let
       p0 = p[0]
@@ -101,78 +136,99 @@ when isMainModule:
     k2 / 2
   proc dK(p:A):A = p
 
-  # Assemble it as Kepler
-  type Kepler = object
+  # Assemble it as TestSys
+  type TestSys = object
     x:A
     p:A
-  proc V(s:Kepler):float = V s.x
-  proc F(s:Kepler):A = F s.x
-  proc K(s:Kepler):float = K s.p
-  proc dK(s:Kepler):A = dK s.p
-  proc status(s:Kepler):string =
+  proc Vc(s:TestSys):float = Vc s.x
+  proc Fc(s:TestSys):A = Fc s.x
+  proc Vl0(s:TestSys):float = Vl0 s.x
+  proc Fl0(s:TestSys):A = Fl0 s.x
+  proc Vl1(s:TestSys):float = Vl1 s.x
+  proc Fl1(s:TestSys):A = Fl1 s.x
+  proc K(s:TestSys):float = K s.p
+  proc dK(s:TestSys):A = dK s.p
+  proc status(s:TestSys):string =
     result = "x: " & $s.x[0] & " " & $s.x[1]
     result &= "  p: " & $s.p[0] & " " & $s.p[1]
     let
-      v = s.V
+      vc = s.Vc
+      vl0 = s.Vl0
+      vl1 = s.Vl1
       k = s.K
-      h = v + k
-    result &= "  V: " & $v & "  K: " & $k & "  H: " & $h
+      h = vc + vl0 + vl1 + k
+    result &= "  Vc: " & $vc & "  Vl0: " & $vl0 & "  Vl1: " & $vl1 & "  K: " & $k & "  H: " & $h
 
   # Basic symplectic update
   type
     MDK = object
-      s:ptr Kepler
-    MDV = object
-      s:ptr Kepler
+      s:ptr TestSys
+    MDVc = object
+      s:ptr TestSys
+    MDVl0 = object
+      s:ptr TestSys
+    MDVl1 = object
+      s:ptr TestSys
+  # For this simple model we overload delta (-dV/dx or dK/dp)
+  proc delta(s:MDK):A = dK s.s[]
+  proc delta(s:MDVc):A = Fc s.s[]
+  proc delta(s:MDVl0):A = Fl0 s.s[]
+  proc delta(s:MDVl1):A = Fl1 s.s[]
   proc evolve(s:MDK, t:float) =
-    let d = dK s.s[]
+    let d = delta s
     s.s.x[0] += t*d[0]
     s.s.x[1] += t*d[1]
-  proc evolve(s:MDV, t:float) =
-    let f = F s.s[]
-    s.s.p[0] += t*f[0]
-    s.s.p[1] += t*f[1]
+  proc evolve(s:MDVc|MDVl0|MDVl1, t:float) =
+    let d = delta s
+    s.s.p[0] += t*d[0]
+    s.s.p[1] += t*d[1]
 
   # Force gradient update
-  proc fgupdate(s:MDV):FGupdate[ptr Kepler] = FGupdate[ptr Kepler](s:s.s)
+  proc fgupdate(s:MDVc|MDVl0|MDVl1):auto = FGupdate[type(s)](s:s)
   proc evolve(s:FGupdate, t:float) =
     # t = 2/3 tau (Yin, 2011)
     let
-      f = F s.s[]
+      s = s.s  # Nim allows us to do this.
+      d = delta s
       h = (3.0/32.0)*t*t  # 1r24=3r32**:2r3
-    # Approximation is valid when h*f is small.
-    var x = s.s.x
-    x[0] += h*f[0]
-    x[1] += h*f[1]
-    let ff = F x
-    s.s.p[0] += t*ff[0]
-    s.s.p[1] += t*ff[1]
+    # Approximation is valid when h*d is small.
+    let x = s.s.x
+    s.s.x[0] += h*d[0]
+    s.s.x[1] += h*d[1]
+    let f = delta s
+    s.s.p[0] += t*f[0]
+    s.s.p[1] += t*f[1]
+    s.s.x = x
 
-  var s:Kepler
+  var s:TestSys
   let
     t = 1.0
     n = 10
-  template runtest(algo, label, ss:untyped) =
+  template runtest(algo0, algo1, algo2, label, ss:untyped) =
+    # algo0/1/2 are the outer, middle, and inner integrators
     echo "######## BEGIN ",label," ########"
     var dHs,dt,er = newseq[float]()
     for steps in ss:
-      # echo "# steps: ",steps
-      let H = algo[MDV,MDK](n:steps, x:MDV(s:s.addr), y:MDK(s:s.addr))
+      if Verb: echo "# steps: ",steps
+      let
+        VK = algo2[MDVc,MDK](n:4, x:MDVc(s:s.addr), y:MDK(s:s.addr))
+        Vl0VK = algo1[MDVl0,type(VK)](n:4, x:MDVl0(s:s.addr), y:VK)
+        H = algo0[MDVl1,type(Vl0VK)](n:steps, x:MDVl1(s:s.addr), y:Vl0VK)
       s.x = [1.0,0]
-      s.p = [0.0,0.75]
-      let h0 = s.V + s.K
+      s.p = [0.0,0.7]
+      let h0 = s.K + s.Vc + s.Vl0 + s.Vl1
       for i in 0..<n:
-        # echo (t*i.float)," ",s.status
+        if Verb: echo (t*i.float)," ",s.status
         H.evolve t
-      # echo (t*n.float)," ",s.status
+      if Verb: echo (t*n.float)," ",s.status
       dt.add(t / steps.float)
-      dHs.add(s.V + s.K - h0)
+      dHs.add(s.K + s.Vc + s.Vl0 + s.Vl1 - h0)
       for i in 0..<n:
         H.evolve(-t)
-      # echo "# reversed to origin: ",s.status
-      let h1 = s.V + s.K
-      er.add((h1-h0)/h0)
-      # echo "\n"
+      if Verb: echo "# reversed to origin: ",s.status
+      let h1 = s.K + s.Vc + s.Vl0 + s.Vl1
+      er.add((h1-h0)/max(abs(h0),abs(h1)))
+      if Verb: echo "\n"
     var m,e = 0.0
     for i in 0..<dt.len:
       if i > 0:  # naive estimate of the error scaling
@@ -189,6 +245,7 @@ when isMainModule:
         quit 1
     echo "# Estimate naive error scale factor: ",m," +/- ",sqrt(e/float((dt.len-1)*(dt.len-2)))
     echo "######## END ",label," ########"
-  runtest(Leapfrog, "Leapfrog", [50,100,200,400,800])
-  runtest(SW92, "SW92", [50,100,200,400,800])
-  runtest(FGYin11, "FGYin11", [25,50,100,200,400])
+  runtest(Leapfrog, Leapfrog, Leapfrog, "Leapfrog", [20,40,80,160,320,640])
+  runtest(SW92, SW92, SW92, "SW92", [20,40,80,160,320,640])
+  runtest(FGYin11, SW92, Leapfrog, "FG/SW/LF", [20,40,80,160,320,640])
+  runtest(FGYin11, FGYin11, FGYin11, "FGYin11", [5,10,20,40,80,160])
